a6e1a30c724286bc788af21544b1614852dd63c0
Jamie Hurst <jamie@jamiehurst.co.uk>
2024-11-16 08:31:31 +0000

    Added simple settings tab

---
+++ src/settings/index.ts
@@ -0,0 +1,20 @@
+export type Period =
+  | 'disabled'
+  | 'daily_9am'
+  | 'daily_11am'
+  | 'daily_3pm'
+  | 'daily_5pm'
+  | 'weekly_mon_9am'
+  | 'weekly_mon_5pm'
+  | 'weekly_fri_9am'
+  | 'weekly_fri_5pm';
+
+export interface ISettings {
+  inbox: boolean;
+  period: Period;
+}
+  
+export const DEFAULT_SETTINGS: ISettings = Object.freeze({
+  inbox: false,
+  period: 'disabled',
+});
