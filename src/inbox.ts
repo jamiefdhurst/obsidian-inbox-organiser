88007f03b05ba4b5dc8d14889b2df5fc9ec8c6e8
Jamie Hurst <jamie@jamiehurst.co.uk>
2024-11-26 08:16:30 +0000

    Very basic screen with notes and dropdowns added

--- src/inbox.ts
+++ src/inbox.ts
@@ -1,4 +1,4 @@
-import { TAbstractFile, Vault } from 'obsidian';
+import { TFile, TFolder, Vault } from 'obsidian';
 import { INBOX_FOLDER } from 'src';
 
 export class Inbox {
@@ -8,12 +8,18 @@ export class Inbox {
     this.vault = vault;
   }
 
-  getFiles(): TAbstractFile[] {
+  getFiles(): TFile[] {
     const folder = this.vault.getFolderByPath(INBOX_FOLDER);
     if (!folder) {
       return [];
     }
 
-    return folder?.children;
+    return folder?.children.filter(child => child instanceof TFile);
+  }
+
+  getFolders(): TFolder[] {
+    const folders = this.vault.getAllFolders(false);
+
+    return folders.filter(folder => folder.path !== INBOX_FOLDER);
   }
  }
